0a62edf53ad6abcdf4bed512a2b04bf8790d1140
Aftab-Ahmed-Soomro <aftab.ahmeddailyideas@gmail.com>
2026-07-14 20:20:21 +0500

    increase a bit more

--- app/components/HomeHero.tsx
+++ app/components/HomeHero.tsx
@@ -1,6 +1,6 @@
 export function HomeHero() {
   return (
-    <section className="relative w-full h-auto aspect-[16/11] sm:aspect-auto sm:h-[100svh] sm:min-h-[550px] max-h-[1080px] flex items-center overflow-hidden">
+    <section className="relative w-full h-auto aspect-[16/12] sm:aspect-auto sm:h-[100svh] sm:min-h-[550px] max-h-[1080px] flex items-center overflow-hidden">
       {/* Background Video */}
       <div className="relative sm:absolute inset-0 w-full h-full z-0">
         <video
