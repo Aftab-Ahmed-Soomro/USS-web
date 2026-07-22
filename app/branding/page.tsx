21f4db62c53d4b63b87e196be8380a9ec0faef7b
Aftab-Ahmed-Soomro <aftab.ahmeddailyideas@gmail.com>
2026-07-22 15:59:47 +0500

    Cta button fixed

--- app/branding/page.tsx
+++ app/branding/page.tsx
@@ -1,4 +1,4 @@
-import { Link } from "lucide-react";
+import Link from "next/link";
 import { Header } from "../components/Header";
 import Stagger from "../components/Stagger";
 import StaggerItem from "../components/Staggeritem";
