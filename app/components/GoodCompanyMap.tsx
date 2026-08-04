d409e89961164224f80a3f4764b2469673478ba8
Aftab-Ahmed-Soomro <aftab.ahmeddailyideas@gmail.com>
2026-08-05 03:31:19 +0500

    done.

--- app/components/GoodCompanyMap.tsx
+++ app/components/GoodCompanyMap.tsx
@@ -10,7 +10,7 @@ export function GoodCompanyMap() {
       src: "/assets/mapLogos/1.png",
       alt: "Precision Talk",
       // Mobile settings (screen < 640px)
-      mobilePos: "top-[68%] left-[77%]",
+      mobilePos: "top-[68%] left-[78%]",
       mobileSize: "w-[45px]",
       // Desktop settings (screen >= 640px)
       desktopPos: "sm:top-[66%] sm:left-[80%]",
@@ -30,7 +30,7 @@ export function GoodCompanyMap() {
       src: "/assets/mapLogos/3.png",
       alt: "Hiba by Amthakara",
       // Mobile settings
-      mobilePos: "top-[58%] left-[30.5%]",
+      mobilePos: "top-[58%] left-[32%]",
       mobileSize: "w-[40px]",
       // Desktop settings
       desktopPos: "sm:top-[58%] sm:left-[34%]",
